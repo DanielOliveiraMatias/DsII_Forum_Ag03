534f99ce45a340972e30f5fda7798d47d06fafec
Samuel Oliveira Matias <samuel.matias01@etec.sp.gov.br>
2025-03-09 13:40:13 -0300

    Adicionado funcionanlidades aos Btns e alterações no Código Fonte

--- assets/js/navbar/toggle_nav.js
+++ assets/js/navbar/toggle_nav.js
@@ -1,8 +1,15 @@
 const nav = document.querySelector('.navbar')
 const toggle = document.querySelectorAll('.toggle_nav')
+const i_toggle = document.querySelectorAll('i .toggle_nav')
 
-toggle.forEach(function(btn) {
-    btn.addEventListener('click', function() {
+toggle.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        nav.classList.toggle('show_nav')
+    })
+})
+
+i_toggle.forEach(function (btn) {
+    btn.addEventListener('click', function () {
         nav.classList.toggle('show_nav')
     })
 })
